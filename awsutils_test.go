df476ade59ac4e2e109ce39188beceed21ad6ffe
Ryan Parman <ryan@ryanparman.com>
2024-09-13 21:13:35 -0600

    Get profiles working.

--- awsutils_test.go
+++ awsutils_test.go
@@ -2,10 +2,9 @@ package awsutils
 
 import (
 	"context"
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func TestGetAWSConfigRegionExplicit(t *testing.T) {
@@ -29,7 +28,7 @@ func TestGetAWSConfigRegionExplicit(t *testing.T) {
 }
 
 func TestGetAWSConfigRegionImplicit1(t *testing.T) {
-	os.Setenv("AWS_REGION", "us-east-1")
+	t.Setenv("AWS_REGION", "us-east-1")
 
 	ctx := context.Background()
 
@@ -50,7 +49,7 @@ func TestGetAWSConfigRegionImplicit1(t *testing.T) {
 }
 
 func TestGetAWSConfigRegionImplicit2(t *testing.T) {
-	os.Setenv("AWS_REGION", "us-east-2")
+	t.Setenv("AWS_REGION", "us-east-2")
 
 	ctx := context.Background()
 
@@ -70,16 +69,50 @@ func TestGetAWSConfigRegionImplicit2(t *testing.T) {
 	}
 }
 
-func TestGetAWSConfigProviderDontPanic(t *testing.T) {
-	assert.NotPanics(t, panicMode, "The code panicked!")
-}
+// Requires a valid AWS profile to be set. It's a bit fragile for anybody but
+// me. Need to move this into GHA with proper automation.
+func TestGetAWSConfigProfile(t *testing.T) {
+	t.Setenv("AWS_PROFILE", "sandbox")
+	t.Setenv("AWS_REGION", "us-east-2")
 
-func panicMode() {
 	ctx := context.Background()
 
-	_, _ = GetAWSConfig(ctx, AWSConfigOptions{
-		Region:  "us-east-1",
+	tmp, err := GetAWSConfig(ctx, AWSConfigOptions{
 		Retries: 3,
 		Verbose: false,
 	})
+	if err != nil {
+		t.Errorf("Error was `%s`.", err)
+	}
+
+	client := sts.NewFromConfig(tmp)
+
+	response, err := client.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		t.Fatalf(
+			"Error with AWS SDK request: If you get this error, try running `aws sso login --sso-session nwl`.\n%s",
+			err,
+		)
+	}
+
+	actual := *response.Account
+	expected := "590184084631"
+
+	if actual != expected {
+		t.Errorf("Result was `%s` instead of `%s`.", actual, expected)
+	}
 }
+
+// func TestGetAWSConfigProviderDontPanic(t *testing.T) {
+// 	assert.NotPanics(t, panicMode, "The code panicked!")
+// }
+
+// func panicMode() {
+// 	ctx := context.Background()
+
+// 	_, _ = GetAWSConfig(ctx, AWSConfigOptions{
+// 		Region:  "us-east-1",
+// 		Retries: 3,
+// 		Verbose: false,
+// 	})
+// }
